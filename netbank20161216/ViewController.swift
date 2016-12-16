61ef5825ec7f7a59099d50a92f57326355d30c86
valtnagy <valtnagy@mislc02nqkmyg3qj.local>
2016-12-16 15:52:09 +0100

    Initial Commit

---
+++ netbank20161216/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  netbank20161216
+//
+//  Created by Nagy, Valter on 2016. 12. 16..
+//  Copyright © 2016. Nagy, Valter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
